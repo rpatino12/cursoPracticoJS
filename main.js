751f5dfe317038f088b7dba82efdc499f000abc2
rpatino12 <rpatino12@outlook.com>
2023-06-30 17:06:34 -0500

    Optimizamos el codigo usando addEventListener

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.getElementById('resultado');
 
+btn.addEventListener('click', calcularOnClick);
+
 function calcularOnClick(){
     const sumaInputs = input1.value + input2.value;
     pResult.innerText = 'Resultado: ' + sumaInputs;
