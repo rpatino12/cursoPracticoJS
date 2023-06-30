d3758a2d9326c70382a35888ab75d651bf0f7a12
rpatino12 <rpatino12@outlook.com>
2023-06-30 17:19:06 -0500

    Optimizamos por segunda vez el codigo

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#formulario')
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.getElementById('resultado');
 
-btn.addEventListener('click', calcularOnClick);
+// Diferentes tipos de EventListeners
+form.addEventListener('submit', sumarInputValues);
+h1.addEventListener('copy', alertaDeCopia);
 
-function calcularOnClick(){
+function sumarInputValues(event){
+    event.preventDefault();
     const sumaInputs = input1.value + input2.value;
     pResult.innerText = 'Resultado: ' + sumaInputs;
+}
+
+function alertaDeCopia(){
+    alert('Copiaste el texto crack!!');
 }
