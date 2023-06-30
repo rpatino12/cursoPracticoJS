1ea43e16c9dc5338d07792175473f9a0cdc369e2
rpatino12 <rpatino12@outlook.com>
2023-06-30 17:25:22 -0500

    Establecimos el comportamiento de calculadora

--- main.js
+++ main.js
@@ -5,16 +5,12 @@ const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.getElementById('resultado');
 
-// Diferentes tipos de EventListeners
 form.addEventListener('submit', sumarInputValues);
-h1.addEventListener('copy', alertaDeCopia);
 
 function sumarInputValues(event){
     event.preventDefault();
-    const sumaInputs = input1.value + input2.value;
-    pResult.innerText = 'Resultado: ' + sumaInputs;
-}
-
-function alertaDeCopia(){
-    alert('Copiaste el texto crack!!');
+    const number1 = parseInt(input1.value);
+    const number2 = parseInt(input2.value);
+    const numbersSum = number1 + number2;
+    pResult.innerText = 'Resultado: ' + numbersSum;
 }
